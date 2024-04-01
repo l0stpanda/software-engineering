fe0024023f9a0dce2d8b337f9d09cbdc8cfc7fd1
Ben Zeng <ben.zeng@live.com>
2024-04-01 15:05:39 -0400

    final push: everything should be fine

--- apps/frontend/src/routes/flowerRequest.tsx
+++ apps/frontend/src/routes/flowerRequest.tsx
@@ -1,65 +1,136 @@
 import React from "react";
-//import axios from "axios";
-import { PostReq } from "../objects/DAO_FlowerReq.ts";
-import { flowerRequestType } from "common/src/flowerRequest.ts";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 
-export default function FlowerRequest() {
-  const [formData, setFormData] = React.useState<flowerRequestType>({
-    room: "",
-    sent_by: "",
-    note: "",
+function FlowerRequest() {
+  type flowerReqFields = {
+    roomNum: string;
+    senderName: string;
+    deliv: string;
+    attachedNote: string;
+  };
+
+  // State for form responses
+  const [responses, setResponses] = useState<flowerReqFields>({
+    roomNum: "",
+    senderName: "",
+    deliv: "",
+    attachedNote: "",
   });
+  const [open, setOpen] = useState(false);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+  // Takes in an event object and updates the responses object when a text field is changed
+  function handleResponseChanges(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setResponses({ ...responses, [e.target.name]: e.target.value });
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    await PostReq(formData);
-    setFormData({ ...formData, room: "", note: "", sent_by: "" });
+  function clear() {
+    setResponses({ roomNum: "", senderName: "", attachedNote: "", deliv: "" });
+  }
+
+  // Clears form, and outputs responses
+  async function handleSubmit() {
+    const res = await axios.post("/api/flowerRequest", responses, {
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(res);
+    setOpen(true);
+  }
+
+  function handleSubmitClose() {
+    setOpen(false);
+    clear();
   }
 
   return (
-    <div></div>
-    // This commented out section is here for your reference when creating a form. Note how the name attribute of each
-    // input field matches the name of the data in the formData type
-    /*
-    <form onSubmit={handleSubmit}>
-      <label>
-        Sent By:
-        <input
-          type="text"
-          name="sent_by"
-          value={formData.sent_by}
-          onChange={handleInputChange}
+    // Your page is everything in this div
+    //<div className="bg-repeat bg-[url('./assets/flowerRequestBackground.png')]">
+    <div className="justify-center grid h-screen place-items-center">
+      <div className="m-auto flex flex-col bg-background rounded-xl px-6 h-fit w-[700px] justify-center py-4">
+        <h1 className="my-2 font-header text-primary font-bold text-3xl text-center">
+          Flower Delivery Request
+        </h1>
+        <div className="flex flex-col gap-4 my-4">
+          <TextField
+            onChange={handleResponseChanges}
+            value={responses.roomNum}
+            type="number"
+            name="roomNum"
+            id="roomNum"
+            variant="filled"
+            label="Room Number"
+            required={true}
+          />
+          <TextField
+            onChange={handleResponseChanges}
+            value={responses.senderName}
+            id="senderName"
+            name="senderName"
+            variant="filled"
+            label="Sent By"
+            placeholder="Name"
+          />
+          <TextField
+            onChange={handleResponseChanges}
+            value={responses.deliv}
+            id="deliv"
+            name="deliv"
+            variant="filled"
+            label="Deliverer"
+            placeholder="Deliverer"
+          />
+        </div>
+        <TextField
+          onChange={handleResponseChanges}
+          value={responses.attachedNote}
+          id="attachedNote"
+          name="attachedNote"
+          variant="filled"
+          label="Note for Patient"
+          multiline={true}
+          maxRows={5}
+          helperText="Optional"
         />
-      </label>
-      <br />
-      <label>
-        Room
-        <input
-          type="text"
-          name="room"
-          value={formData.room}
-          onChange={handleInputChange}
-        />
-      </label>
-      <br />
-      <label>
-        Note
-        <input
-          type="text"
-          name="note"
-          value={formData.note}
-          onChange={handleInputChange}
-        />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-    */
+        <Button
+          className="w-32 self-center"
+          onClick={handleSubmit}
+          type="submit"
+          id="requestSubmit"
+          variant="contained"
+          size="large"
+          sx={{ borderRadius: "30px" }}
+        >
+          SUBMIT
+        </Button>
+      </div>
+      <Dialog open={open} onClose={handleSubmitClose}>
+        <DialogTitle>We received your request!</DialogTitle>
+        <DialogContent>
+          <strong>Here are your responses:</strong>
+          <br />
+          Room Number: {responses.roomNum}
+          <br />
+          Sent By: {responses.senderName}
+          <br />
+          Note for Patient: {responses.attachedNote}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleSubmitClose} autoFocus>
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    //</div>
   );
 }
+
+export default FlowerRequest;
