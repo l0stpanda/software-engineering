9e14195e89934927986b494b55c6fc0bb4c4277e
abroche <abroche24@yahoo.com>
2024-04-01 14:26:08 -0400

    commit more files

--- apps/frontend/src/routes/ExampleRoute.tsx
+++ apps/frontend/src/routes/ExampleRoute.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ExampleComponent } from "../components/ExampleComponent.tsx";
-
+//import SingleFileUploader from "../components/fileUploader.tsx";
+//<SingleFileUploader />
 export default function ExampleRoute() {
   return (
     <div className="w-100">
