f534966799e1a7b205e29361da35da886c8705d7
kgkhs001 <kgarg@wpi.edu>
2024-04-02 17:59:57 -0400

    Download on the imp page now

--- apps/frontend/src/routes/DownloadCSV.tsx
+++ apps/frontend/src/routes/DownloadCSV.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import SingleDownloader from "../components/fileDownload.tsx";
+//import SingleDownloader from "../components/fileDownload.tsx";
 
 export default function ReadRoute() {
   return (
     <div className="imp">
       <h1>Download file</h1>
-      <SingleDownloader></SingleDownloader>
     </div>
   );
 }
