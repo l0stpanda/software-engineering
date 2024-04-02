e6d6bcac17b861c6d8621bbcfb20823d8b34655d
samnaik10 <samnakem@gmail.com>
2024-04-01 23:14:45 -0400

    hero page updated with minor edits left

---
+++ apps/frontend/src/routes/heroPage.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './heroPage.css';
+import {Button} from "@mui/material";
+import mapImg from "../assets/LL1Map.png";
+
+export default function heroPage() {
+  return (
+      <div className="wholePage">
+          <div className="header">
+              <h3 className="sHead">Brigham and Women's Hospital</h3>
+              <h1 className="bHead">Transforming Medicine <br/>Through Life-giving <br/>Breakthroughs</h1>
+          </div>
+          <div className="heroSecondCol">
+            <button className="touchToStart">
+                <h1 className="touchStartText"><strong>Touch here to view the map</strong></h1>
+            </button>
+            <div className="mapPreviewBox">
+                <img src={mapImg} className="mapPreview" alt="Map Preview"/>
+            </div>
+            <div className="loginButtonBox">
+                <p>Staff Member?</p>
+                <Button variant="contained" sx={{borderRadius: "30px"}}>
+                    Login
+                </Button>
+            </div>
+          </div>
+      </div>
+  );
+}
