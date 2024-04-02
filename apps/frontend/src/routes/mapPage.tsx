38e741ce0d6863f8c251ef90966af09a7b6163bb
Alessandra Giovenco <amgiovenco@wpi.edu>
2024-04-01 21:15:25 -0400

    Updated Map Page :) hopefully this one works sorry carter

--- apps/frontend/src/routes/mapPage.tsx
+++ apps/frontend/src/routes/mapPage.tsx
@@ -6,12 +6,13 @@ import {
   TransformComponent,
   useControls,
 } from "react-zoom-pan-pinch";
+import { useState } from "react";
 
 function Map() {
   const Controls = () => {
     const { zoomIn, zoomOut } = useControls();
     return (
-      <div className="fixed pt-10 px-3 z-10 flex gap-2">
+      <div className="absolute pt-10 px-3 z-10 flex gap-2">
         <Button
           onClick={() => zoomIn()}
           type="button"
@@ -38,19 +39,39 @@ function Map() {
     );
   };
 
+  const [navigatingNodes, setNavigatingNodes] = useState({
+    start: "",
+    end: "",
+  });
+
+  function handleFormChanges(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setNavigatingNodes({ ...navigatingNodes, [name]: value });
+  }
+
+  function handleFormSubmit() {
+    // Call navigation function with form responses
+    return;
+  }
+
   return (
     <div>
-      {/*Location and Destination Box*/}
+      {/*Location and Destination things*/}
       <div
         className="my-8
                    flex
                    flex-row-reverse
                    min-h-screen
-                   max-w-screen-2xl
-                   justify-start"
+                   max-w-screen-2xl"
       >
-        {/*Map Image*/}
-        <div className="w-2/3">
+        {/*Map Image Box*/}
+        <div
+          className="w-2/3
+        h-2/3
+        flex
+        border-primary
+        border-2"
+        >
           <TransformWrapper>
             <Controls />
             <TransformComponent>
@@ -59,7 +80,7 @@ function Map() {
           </TransformWrapper>
         </div>
 
-        {/*headers and stuff*/}
+        {/*boxes.*/}
         <div
           className="flex flex-col
                 w-1/4
@@ -77,7 +98,7 @@ function Map() {
                     border-primary
                     border-2"
           >
-            <div className="">
+            <div className="flex flex-col justify-center">
               <h2 className="text-primary font-header pb-4">
                 Where would you like to go?
               </h2>
@@ -85,27 +106,56 @@ function Map() {
                 id="outlined-basic"
                 variant="outlined"
                 label="Location"
+                value={navigatingNodes.start}
+                onChange={handleFormChanges}
                 sx={{
                   input: {
                     color: "primary",
                     background: "background",
                   },
                 }}
+                margin="normal"
               />
-              <br />
               <TextField
                 id="outlined-basic"
                 variant="outlined"
                 label="Destination"
+                value={navigatingNodes.end}
+                onChange={handleFormChanges}
                 sx={{
                   input: {
                     color: "primary",
                     background: "background",
                   },
                 }}
+                margin="normal"
               />
+              <Button
+                type="button"
+                variant="contained"
+                className="submitButton"
+                size="medium"
+                onClick={handleFormSubmit}
+                sx={{ borderRadius: "30px" }}
+              >
+                Submit
+              </Button>
             </div>
           </div>
+          {/*second non-functional box for rn*/}
+          <div
+            className="mr-8
+                        ml-5
+                        mt-5
+                        py-5
+                        flex
+                        flex-col
+                        items-center
+                        bg-background
+                        rounded-xl
+                        border-primary
+                        border-2"
+          ></div>
         </div>
       </div>
     </div>
