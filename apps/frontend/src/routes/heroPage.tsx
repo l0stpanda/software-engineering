e2254435198a10a2f5a4b89e5ec074e97f8b490f
l0stpanda <ben.zeng@live.com>
2024-04-02 21:06:47 -0400

    final commit

--- apps/frontend/src/routes/heroPage.tsx
+++ apps/frontend/src/routes/heroPage.tsx
@@ -2,7 +2,6 @@ import "./heroPage.css";
 import mapImg from "../assets/LL1Map.png";
 import React, { useState } from "react";
 import {
-  Button,
   Dialog,
   //    Dialog,
   //    DialogActions,
@@ -44,39 +43,9 @@ function HeroPage() {
         <div className="mapPreviewBox">
           <img src={mapImg} className="mapPreview" alt="Map Preview" />
         </div>
-        <div className="loginButtonBox">
-          <p className="self-center" style={{ color: "white" }}>
-            Staff Member?
-          </p>
-          <Button
-            variant="contained"
-            sx={{ borderRadius: "30px" }}
-            onClick={setLoginOpen}
-            className="self-center"
-          >
-            Login
-          </Button>
-
-          {/*<Dialog open={open} onClose={handleSubmitClose}>*/}
-          {/*    <DialogTitle>We received your request!</DialogTitle>*/}
-          {/*    <DialogContent>*/}
-          {/*        <strong>Here are your responses:</strong>*/}
-          {/*        <br />*/}
-          {/*        Room Number: {responses.roomNum}*/}
-          {/*        <br />*/}
-          {/*        Sent By: {responses.senderName}*/}
-          {/*        <br />*/}
-          {/*        Send To: {responses.sendTo}*/}
-          {/*        <br />*/}
-          {/*        Note for Patient: {responses.attachedNote}*/}
-          {/*    </DialogContent>*/}
-          {/*    <DialogActions>*/}
-          {/*        <Button onClick={handleSubmitClose} autoFocus>*/}
-          {/*            Okay*/}
-          {/*        </Button>*/}
-          {/*    </DialogActions>*/}
-          {/*</Dialog>*/}
-        </div>
+        <button className="loginButtonBox" onClick={setLoginOpen}>
+          <h1>Staff Member? Login Here</h1>
+        </button>
       </div>
       <Dialog open={dialogOpen} onClose={setLoginClose}>
         <LoginDialog />
