766fe405394944ae499e22773c93252aea70bb5a
kgkhs001 <kgarg@wpi.edu>
2024-04-03 22:34:54 -0400

    Changes by carterm

--- apps/frontend/src/routes/readRouteE.tsx
+++ apps/frontend/src/routes/readRouteE.tsx
@@ -5,7 +5,6 @@ import SingleFileUploader from "../components/readAndDisCSV.tsx";
 export default function ImportRouteE() {
   return (
     <div className="imp bg-background">
-      <h1>Display file</h1>
       <SingleFileUploader></SingleFileUploader>
     </div>
   );
