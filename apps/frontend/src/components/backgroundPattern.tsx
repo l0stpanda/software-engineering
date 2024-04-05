fde92dfa04a124b1240d6ae89c4bf6f154237f81
CraterMore <ctm6704@yahoo.com>
2024-04-05 17:27:48 -0400

    Fixed background pattern image not loading

--- apps/frontend/src/components/backgroundPattern.tsx
+++ apps/frontend/src/components/backgroundPattern.tsx
@@ -2,7 +2,7 @@
 
 function backgroundPattern() {
   return (
-    <div className="absolute w-full h-full bg-repeat bg-[url('../assets/PatternBackground-Test.png')] -z-10"></div>
+    <div className="absolute w-full h-full bg-repeat bg-[url('src/assets/PatternBackground-Test.png')] -z-10"></div>
   );
 }
 
