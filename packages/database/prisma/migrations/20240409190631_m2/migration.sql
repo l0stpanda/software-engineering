4c34da11bff4bb2132349305dc4dbac6bda4edeb
CraterMore <ctm6704@yahoo.com>
2024-04-09 17:17:18 -0400

    SQL migration

--- packages/database/prisma/migrations/20240409190631_m2/migration.sql
+++ packages/database/prisma/migrations/20240409190631_m2/migration.sql
@@ -8,6 +8,19 @@ CREATE TABLE "HighScore" (
 );
 
 -- CreateTable
+CREATE TABLE "RoomScheduler" (
+    "id" SERIAL NOT NULL,
+    "employName" TEXT NOT NULL,
+    "startTime" TEXT NOT NULL,
+    "lengthRes" TEXT NOT NULL,
+    "roomNum" TEXT NOT NULL,
+    "reqStatus" TEXT NOT NULL,
+    "priority" TEXT NOT NULL,
+
+    CONSTRAINT "RoomScheduler_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Flowers" (
     "id" SERIAL NOT NULL,
     "room" TEXT NOT NULL,
