10d8c6b4bddec731af8dc1b89129eaa344023739
VincentHuang <vincenthuang063@gmail.com>
2024-04-09 19:51:54 -0400

    heroPage edit and in the middle of doing importExport page

--- apps/frontend/src/routes/importRouteE.tsx
+++ apps/frontend/src/routes/importRouteE.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import SingleFileUploader from "../components/fileUploader.tsx";
+import SingleDownloader from "../components/fileDownload.tsx";
 
-export default function ImportRouteE() {
+export default function ImportExportRoute() {
   return (
     <div>
-      <SingleFileUploader></SingleFileUploader>
+      <h2>Import and Export</h2>
+      <div>
+        <SingleFileUploader />
+      </div>
+      <div>
+        <SingleDownloader />
+      </div>
     </div>
   );
 }
