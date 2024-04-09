d28d7d00dd1433028cd409f40884427eef1fab5c
abroche <abroche24@yahoo.com>
2024-04-08 20:29:37 -0400

    Adding comments and moving stuff around.

--- apps/frontend/src/components/FloorNode.tsx
+++ apps/frontend/src/components/FloorNode.tsx
@@ -228,14 +228,18 @@ function FloorNode(props: FloorNodesProps) {
 }
 
 function getFloorByImage(imgName: string): string {
+  // split the image path name by /
   const parts = imgName.split("/");
+  // obtain the last part (e.g., 00_thelowerlevel1.png)
   const lastpart = parts[parts.length - 1];
+  // split based on underscore
   const splits = lastpart.split("_");
+  //take the number
   const numbers = splits[0][1];
-  const onlyName = splits[1].slice(0, -4);
-  //console.log(onlyName);
-  //console.log(numbers);
+
   if (numbers == "0") {
+    // get only the name without the .png
+    const onlyName = splits[1].slice(0, -4);
     if (
       onlyName.includes("thegroundfloor") ||
       onlyName.includes("thelowerlevel1")
