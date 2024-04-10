973e0dd3959a9f2560d2adb209d24830ebcbed97
qs223 <qchen5@wpi.edu>
2024-04-09 20:43:54 -0400

    combine file download and upload into one page

--- apps/frontend/src/routes/importRouteE.tsx
+++ apps/frontend/src/routes/importRouteE.tsx
@@ -1,23 +1,12 @@
 import React from "react";
-import SingleFileUploader from "../components/fileUploader.tsx";
-import SingleDownloader from "../components/fileDownload.tsx"; // Import the downloader component
+// import SingleFileUploader from "../components/fileUploader.tsx";
+// import SingleDownloader from "../components/fileDownload.tsx"; // Import the downloader component
+import DownloadAndUploader from "../components/fileDownloadAndUploader.tsx";
 
 export default function importRouteE() {
   return (
     <div className="space-y-4">
-      {" "}
-      {/* Add some spacing between components */}
-      <h2 className="text-center text-2xl font-semibold">
-        Import and Export Files
-      </h2>
-      <div>
-        <h3 className="text-lg font-semibold">Import</h3>
-        <SingleFileUploader />
-        <SingleDownloader />
-      </div>
-      <div>
-        <h3 className="text-lg font-semibold">Export</h3>
-      </div>
+      <DownloadAndUploader />
     </div>
   );
 }
