962fedec525c66019c0132e8e67606fa92703685
Krishna Garg <54680043+kgkhs001@users.noreply.github.com>
2024-04-30 22:59:38 -0400

    Shit be working now?

--- packages/database/prisma/migrations/20240430235549_new_name_again/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `emp_name` on table `GeneralService` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "GeneralService" ALTER COLUMN "emp_name" SET NOT NULL;
